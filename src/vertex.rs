5c806112aaa202c9d1f0b7c32ad8794dee71cc05
Lukas Weber <dev@sakul-flee.de>
2023-07-08 17:01:47 +0200

    Fixed deprecation of Vertex

--- src/vertex.rs
+++ src/vertex.rs
@@ -1,9 +1,9 @@
 use bytemuck::{Pod, Zeroable};
+use vulkano::pipeline::graphics::vertex_input::Vertex as VVertex;
 
 #[repr(C)]
-#[derive(Clone, Copy, Debug, Default, Zeroable, Pod)]
+#[derive(Clone, Copy, Debug, Default, Zeroable, Pod, VVertex)]
 pub struct Vertex {
+    #[format(R32G32_SFLOAT)]
     pub position: [f32; 2],
 }
-
-vulkano::impl_vertex!(Vertex, position);
